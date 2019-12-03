9c15335c5a81947cc830766a42e72a74d07bf065
김지환 <pptwenty@gmail.com>
2019-12-03 16:10:10 +0900

    심플 비밀투표 - 설문 목록 노출및 조회 기능 생성

---
+++ src/main/apps/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SurveyApp from './js/SurveyApp';
+
+ReactDOM.render(<SurveyApp />, document.getElementById('root'));
---
+++ src/main/apps/src/js/SurveyApp.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {makeStyles} from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import SurveyList from './SurveyList';
+
+function SurveyPanel(props) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <Typography
+            component="div"
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            <Box p={3}>{children}</Box>
+        </Typography>
+    );
+}
+
+SurveyPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.any.isRequired,
+    value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+    return {
+        id: `simple-tab-${index}`,
+        'aria-controls': `simple-tabpanel-${index}`,
+    };
+}
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.paper,
+    },
+}));
+
+export default function SurveyApp() {
+    const classes = useStyles();
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+        console.log(newValue);
+    };
+
+    return (
+        <div className={classes.root}>
+            <AppBar position="static">
+                <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
+                    <Tab label="Do Survey" {...a11yProps(0)} />
+                    <Tab label="Confirm Result" {...a11yProps(1)} />
+                </Tabs>
+            </AppBar>
+            <SurveyPanel value={value} index={0}>
+                <SurveyList/>
+            </SurveyPanel>
+            <SurveyPanel value={value} index={1}>
+                Confirm Result
+            </SurveyPanel>
+        </div>
+    );
+}
+
